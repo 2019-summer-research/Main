845c834682b9ea7a7f4921d2b441cbcb72b2d0e9
Conrad Weiser <vibrantclouds@protonmail.com>
2019-06-06 21:00:32 -0400

    Api framework implemented

---
+++ ResearchTests/src/main/java/api/RequestManager.java
@@ -0,0 +1,169 @@
+package api;
+
+import api.parameters.HttpHeaderParameter;
+import api.parameters.UrlParameter;
+import com.google.gson.Gson;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+/**
+ * A class which handles making API transactions to outside sources.
+ * This class is implemented as Singleton to ensure that requests are made in a synchronous fashion, thus negating
+ * any concerns about some requests externally processing faster than others which may cause errors depending
+ * on the cloud APIs which are used.
+ */
+public class RequestManager {
+
+	/**
+	 * Singleton instance of this class
+	 */
+	private static RequestManager instance = null;
+
+	/**
+	 * The OkHttp API instance used to execute the managed API calls
+	 */
+	OkHttpClient okClient = new OkHttpClient();
+
+	/**
+	 * Singleton accessor to the API Request Manager
+	 * @return the only active instance of this class
+	 */
+	public static RequestManager getInstance() {
+		if(RequestManager.instance == null) {
+			RequestManager.instance = new RequestManager();
+		}
+		return RequestManager.instance;
+	}
+
+	public <T> T makeApiRequest(ApiMethod method) {
+		T response = null;
+
+		// Which request type are we using? Delegate it to the proper method
+		switch(method.getMethod()) {
+			case DELETE:
+				break;
+			case GET:
+				break;
+			case HEAD:
+				break;
+			case OPTIONS:
+				break;
+			case POST: response = this.makePostRequest(method);
+				break;
+			case PUT:  response = this.makePutRequest(method);
+				break;
+			case TRACE:
+				break;
+			default:
+				break;
+
+		}
+
+		return response;
+	}
+
+	/**
+	 * Method which uses OKHTTP to send a POST request to the specified URL saved within the @link{ApiMethod} parameter
+	 */
+	private <T> T makePostRequest(ApiMethod method) {
+		RequestBody body = RequestBody.create(method.getMediaType(), method.getBody());
+		Request.Builder request = new Request.Builder();
+
+		// Append each of the headers for this method
+		for (HttpHeaderParameter currentHeader : method.getHttpHeaderParameters()) {
+			request.addHeader(currentHeader.getKey(), currentHeader.getValue());
+		}
+
+		try {
+			// Append the request body
+			Request builtRequest = request.url(method.getUrl())
+					.post(body)
+					.build();
+
+			// Make the request
+			Response response = okClient.newCall(builtRequest).execute();
+
+			// Parse the response with Gson
+			Gson gson = new Gson();
+			String responseJsonString = response.body().string();
+
+			// If the response type for this is VOID (Meaning we are not expecting a response) do not try to use Gson
+			if(method.getReturnType() == Void.TYPE)
+				return (T) Void.TYPE;
+
+			T data = gson.fromJson(responseJsonString, method.getReturnType());
+			return data;
+
+		} catch (IOException ex) {
+			// Create a specific error message for this problem and print it to the err-out
+			StringBuilder builder = new StringBuilder();
+			builder.append("[Error] - POST request failed.\nBase URL:");
+			builder.append(method.getBaseUrl());
+			builder.append("Parameters used: ");
+			for(UrlParameter param : method.getUrlParameters()) {
+				builder.append(param.toString()).append("\n");
+			}
+			builder.append("\n");
+
+			System.err.println(builder.toString());
+
+		}
+
+		return null;
+	}
+
+	/**
+	 * Method which uses OKHTTP to send a POST request to the specified URL saved within the @link{ApiMethod} parameter
+	 */
+	private <T> T makePutRequest(ApiMethod method) {
+		RequestBody body = RequestBody.create(method.getMediaType(), method.getBody());
+		Request.Builder request = new Request.Builder();
+
+		// Append each of the headers for this method
+		for (HttpHeaderParameter currentHeader : method.getHttpHeaderParameters()) {
+			request.addHeader(currentHeader.getKey(), currentHeader.getValue());
+		}
+
+		try {
+			// Append the request body
+			Request builtRequest = request.url(method.getUrl())
+					.put(body)
+					.build();
+
+			// Make the request
+			Response response = okClient.newCall(builtRequest).execute();
+
+			// Parse the response with Gson
+			Gson gson = new Gson();
+			String responseJsonString = response.body().string();
+
+			// If the response type for this is VOID (Meaning we are not expecting a response) do not try to use Gson
+			if(method.getReturnType() == Void.TYPE)
+				return (T) Void.TYPE;
+
+			T data = gson.fromJson(responseJsonString, method.getReturnType());
+			return data;
+
+		} catch (IOException ex) {
+			// Create a specific error message for this problem and print it to the err-out
+			StringBuilder builder = new StringBuilder();
+			builder.append("[Error] - POST request failed.\nBase URL:");
+			builder.append(method.getBaseUrl());
+			builder.append("Parameters used: ");
+			for(UrlParameter param : method.getUrlParameters()) {
+				builder.append(param.toString()).append("\n");
+			}
+			builder.append("\n");
+
+			System.err.println(builder.toString());
+
+		}
+
+		return null;
+	}
+
+}
---
+++ ResearchTests/src/main/java/api/parameters/UrlParameter.java
@@ -0,0 +1,66 @@
+package api.parameters;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+/**
+ * Class which stores information of HTTP UrlParameters to be encoded in transactions
+ */
+public class UrlParameter {
+
+	private final String key;
+	private final String value;
+
+	/**
+	 * Parameters must be encoded as strings for HTTP transactions. This constructor stores such values.
+	 * Each of the constructor overloads merely converts the data types into String and invokes this method
+	 * @param key
+	 * @param value
+	 */
+	public UrlParameter(String key, String value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	public UrlParameter(String key, int value) {
+		this(key, String.valueOf(value));
+	}
+
+	public UrlParameter(String key, long value) {
+		this(key, String.valueOf(value));
+	}
+
+	public UrlParameter(String key, boolean value) {
+		this(key, value ? "true" : "false");
+	}
+
+	public UrlParameter(String key, Object value) {
+		this(key, value.toString());
+	}
+
+	protected String getKey() {
+		return this.key;
+	}
+
+	protected String getValue() {
+		return this.value;
+	}
+
+	/**
+	 * ToString needs to convert this parameter into the proper format which can be used in a URL.
+	 * Example: Key = Shape, Value = Square --> "Shape=Square" (In UTF-8 Encoding)
+	 * @return the string representation of this URL parameter which can be inserted safely into a URL request
+	 */
+	@Override
+	public String toString() {
+		String parameter = null;
+		try {
+			parameter = URLEncoder.encode(getKey(), "UTF-8") + "=" + URLEncoder.encode(getValue(), "UTF-8");
+		} catch (UnsupportedEncodingException ex) {
+			//I'm really impressed you actually got to this bit of code, since the encoding is hardcoded...
+			ex.printStackTrace();
+		}
+		return parameter;
+	}
+
+}
