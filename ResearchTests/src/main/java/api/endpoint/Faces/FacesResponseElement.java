0654622a43fe35a575296b28bacd77a04d7d4fe6
Unknown <snowy6409@comcast.net>
2019-06-07 15:01:55 -0400

    Added Packages for other API endpoint calls

---
+++ ResearchTests/src/main/java/api/endpoint/Faces/FacesResponseElement.java
@@ -0,0 +1,10 @@
+package api.endpoint.Faces;
+
+import java.util.HashMap;
+
+public class FacesResponseElement {
+
+    HashMap<String, String> error;
+    String persistedFaceId;
+
+}
---
+++ ResearchTests/src/main/java/api/endpoint/Person/PersonResponseElement.java
@@ -0,0 +1,10 @@
+package api.endpoint.Person;
+
+import java.util.HashMap;
+
+public class PersonResponseElement {
+    HashMap<String, String> error;
+    String personId;
+
+
+}
